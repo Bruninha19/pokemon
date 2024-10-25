9ff1ec1787187909e940a2097063df41dddaf8b9
Bruninha19 <140767534+Bruninha19@users.noreply.github.com>
2024-10-25 20:17:32 -0300

    Update pokemonModel.js

--- models/pokemonModel.js
+++ models/pokemonModel.js
@@ -2,6 +2,7 @@ const pokemons = [
     {id: 1, nome: 'Bulbassauro', tipo: 'Vegetal/Veneno'},
     {id: 2, nome: 'Squirtle', tipo: 'Água'},
     {id: 3, nome: 'Charmander', tipo: 'Fogo'},
+    {id: 4, nome: 'Pikachu', tipo: 'Elétrico'},
 ];
 
 const getPokemons = () => pokemons;
